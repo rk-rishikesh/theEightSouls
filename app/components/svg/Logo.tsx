0966013648fabd44d30f5fb308fb590f04844d79
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2023-12-09 19:19:04 +0530

    Added TBA

--- app/components/svg/Logo.tsx
+++ app/components/svg/Logo.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 interface LogoProps {
   width?: number;
@@ -8,12 +7,10 @@ interface LogoProps {
 
 const Logo: React.FC<LogoProps> = ({ width = 100, height = 100 }) => {
     return (
-        <Image
+        <img
             className={`w-${width} h-${height}`}
             src="/logo.svg"
             alt="Logo"
-            width={width}
-            height={height}
         />
     );
 };
